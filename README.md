628acb89cfeee7fb9cba0e75047960b5f2ec94fb
Brian Bantigue <bignatue@gmail.com>
2020-09-17 16:51:58 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Netflix Clone
+
+Cloning Netflix following Clever Programmer tutorial.
+
+![Design preview for Relaxer App](./preview.jpg)
+
+Content was from themoviedb api.
+
+Tools used:
+- axios
+- useState
+- useEffect
+- movie-trialer
+- movie-youtube
---
+++ precache-manifest.e75eca9f40372ce577deb5d83f568501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8e44f46a7373d2b8cf516bf37a71aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6f71fab7eddfdccd91c",
+    "url": "/static/css/main.9bc8b0f1.chunk.css"
+  },
+  {
+    "revision": "36244f9f8a0b4471123d",
+    "url": "/static/js/2.1b28dccb.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.1b28dccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f71fab7eddfdccd91c",
+    "url": "/static/js/main.38cde272.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
